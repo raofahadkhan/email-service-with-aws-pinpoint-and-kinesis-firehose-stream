808ee21ed5fc7f2f4a4a0ee780ac79fa023c2ff3
Rao Fahad <raofahad046@gmail.com>
2023-10-14 23:52:50 +0500

    create s3 bucket for saving emails data to s3

--- lib/aws-pinpoint-email-service-stack.ts
+++ lib/aws-pinpoint-email-service-stack.ts
@@ -1,11 +1,16 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class AwsPinpointEmailServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
     const { service, stage } = props?.tags!;
+
+    const pinpointEmailsInsightsBuckets = new s3.Bucket(this, `${service}-${stage}-bucket`, {
+      bucketName: `${service}-${stage}-bucket`,
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
