512fc007f630e7cbcf622ce0e712e2afa14c1301
Rao Fahad <raofahad046@gmail.com>
2023-10-15 00:25:18 +0500

    create the http api for sending emails

--- lib/aws-pinpoint-email-service-stack.ts
+++ lib/aws-pinpoint-email-service-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as apigwv2 from "@aws-cdk/aws-apigatewayv2-alpha";
 
 export class AwsPinpointEmailServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,6 +12,17 @@ export class AwsPinpointEmailServiceStack extends cdk.Stack {
       bucketName: `${service}-${stage}-bucket`,
       versioned: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const pinpointEmailApi = new apigwv2.HttpApi(this, `${service}-${stage}-api`, {
+      apiName: `${service}-${stage}-api`,
+      description: "This api is responsible for sending emails with pinpoint.",
+      corsPreflight: {
+        allowHeaders: ["Content-Type"],
+        allowMethods: [apigwv2.CorsHttpMethod.POST],
+        allowCredentials: false,
+        allowOrigins: ["*"],
+      },
     });
   }
 }
