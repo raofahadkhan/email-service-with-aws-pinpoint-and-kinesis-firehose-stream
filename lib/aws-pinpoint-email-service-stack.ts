c23764a9c77d32dd80849f14b77c522616aee0a0
Rao Fahad <raofahad046@gmail.com>
2023-10-15 00:47:40 +0500

    created a pinpoint app

--- lib/aws-pinpoint-email-service-stack.ts
+++ lib/aws-pinpoint-email-service-stack.ts
@@ -5,6 +5,7 @@ import * as apigwv2 from "@aws-cdk/aws-apigatewayv2-alpha";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 import * as apigwv2_integrations from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import * as pinpoint from "aws-cdk-lib/aws-pinpoint";
 
 export class AwsPinpointEmailServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -26,6 +27,10 @@ export class AwsPinpointEmailServiceStack extends cdk.Stack {
         allowCredentials: false,
         allowOrigins: ["*"],
       },
+    });
+
+    const pinpointEmailApp = new pinpoint.CfnApp(this, `${service}-${stage}-project`, {
+      name: `${service}-${stage}-project`,
     });
 
     const pinpointSendEmailLambda = new lambda.Function(
@@ -60,6 +65,10 @@ export class AwsPinpointEmailServiceStack extends cdk.Stack {
       path: "/",
       methods: [apigwv2.HttpMethod.POST],
       integration: pinpointSendEmailLambdaIntegration,
+    });
+
+    new cdk.CfnOutput(this, `${service}-${stage}-feedback-api-endpoint`, {
+      value: pinpointEmailApi.url!,
     });
   }
 }
