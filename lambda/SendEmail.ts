e2ca7ebabfdd3e9332511be8567b06f116050f98
Rao Fahad <raofahad046@gmail.com>
2023-10-24 02:44:53 +0500

    removed extra code

--- lambda/SendEmail.ts
+++ lambda/SendEmail.ts
@@ -2,41 +2,10 @@ const AWS = require("aws-sdk");
 const ses = new AWS.SES();
 import { SendEmailRequest } from "aws-sdk/clients/ses";
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
-const dynamodb = new AWS.DynamoDB.DocumentClient();
-// import { v4 as uuidv4 } from "uuid";
 
 export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
-//   if (!event.body) {
-//     return {
-//       statusCode: 400,
-//       body: JSON.stringify({ message: "Body is missing" }),
-//     };
-//   }
-
-//   const requestBody = JSON.parse(event.body);
-  //   const user_id: string = requestBody.user_id;
-  //   const feedback_id: string = uuidv4();
-  //   const feedback_message: string = requestBody.feedback;
   const TO_EMAIL: string = process.env.TO_EMAIL!;
   const FROM_EMAIL: string = process.env.FROM_EMAIL!;
-
-  const isValidEmail = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-
-  //   if (!user_id.match(isValidEmail)) {
-  //     return {
-  //       statusCode: 400,
-  //       body: JSON.stringify({ message: "email is not valid" }),
-  //     };
-  //   }
-
-  //   const params = {
-  //     TableName: process.env.TABLE_NAME,
-  //     Item: {
-  //       user_id: user_id,
-  //       feedback_id: feedback_id,
-  //       feedback_message: feedback_message,
-  //     },
-  //   };
 
   const sesParams: SendEmailRequest = {
     Destination: {
@@ -70,7 +39,7 @@ export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGateway
     <div class="card">
       <div class="container">
       <h1>Feedback From Customer</h1>
-      <p>Feedback Message :<a src="https://www.google.com">Fahad's Profile Link</a></p> 
+      <p>Feedback Message :<a href="https://www.google.com">Fahad's Profile Link</a></p> 
       </div>
     </div>
     </body>
@@ -86,7 +55,6 @@ export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGateway
   };
 
   try {
-    // await dynamodb.put(params).promise();
     await ses.sendEmail(sesParams).promise();
     return {
       statusCode: 200,
