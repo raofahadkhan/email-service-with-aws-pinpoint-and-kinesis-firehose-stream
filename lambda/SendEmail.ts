60b119d55f366fcf05547773dfb82e412aba9702
RAO FAHAD <raofahad046@gmail.com>
2023-12-09 01:06:42 +0500

    created a lambda function for sending emails through pinpoint

--- lambda/SendEmail.ts
+++ lambda/SendEmail.ts
@@ -1,61 +1,42 @@
-const AWS = require("aws-sdk");
-const ses = new AWS.SES();
-import { SendEmailRequest } from "aws-sdk/clients/ses";
+import { Pinpoint } from "aws-sdk";
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import { emailContent } from "./email";
+const pinpoint = new Pinpoint();
 
-export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+export const handler = async (
+  event: APIGatewayProxyEventV2
+): Promise<APIGatewayProxyResultV2> => {
   const TO_EMAIL: string = process.env.TO_EMAIL!;
   const FROM_EMAIL: string = process.env.FROM_EMAIL!;
+  const applicationId: string = process.env.APP_ID!;
+  const emailSubject: string = "Pinpoint Testing Email";
 
-  const sesParams: SendEmailRequest = {
-    Destination: {
-      ToAddresses: [TO_EMAIL],
-    },
-    Message: {
-      Body: {
-        Html: {
-          Data: `
-          <!DOCTYPE html>
-          <html lang="en">
-    <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-    .card {
-      box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-      transition: 0.3s;
-      width: 80%;
-    }
-    
-    .card:hover {
-      box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-    }
-    
-    .container {
-      padding: 2px 16px;
-    }
-    </style>
-    </head>
-    <body>
-    <div class="card">
-      <div class="container">
-      <h1>Feedback From Customer</h1>
-      <p>Feedback Message :<a href="https://www.google.com">Fahad's Profile Link</a></p> 
-      </div>
-    </div>
-    </body>
-          </html>
-          `,
+  const paramsForEmail = {
+    ApplicationId: applicationId,
+    MessageRequest: {
+      Addresses: {
+        [FROM_EMAIL]: { ChannelType: "EMAIL" },
+      },
+      MessageConfiguration: {
+        EmailMessage: {
+          SimpleEmail: {
+            HtmlPart: {
+              Charset: "UTF-8",
+              Data: emailContent,
+            },
+            Subject: {
+              Charset: "UTF-8",
+              Data: emailSubject,
+            },
+          },
+          FromAddress: TO_EMAIL,
         },
       },
-      Subject: {
-        Data: "Feed back Message from Customer",
-      },
     },
-    Source: FROM_EMAIL,
   };
 
   try {
-    await ses.sendEmail(sesParams).promise();
+    await pinpoint.sendMessages(paramsForEmail).promise();
     return {
       statusCode: 200,
       body: JSON.stringify({
---
+++ lambda/email.ts
@@ -0,0 +1,29 @@
+export const emailContent = `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.card {
+box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+transition: 0.3s;
+width: 80%;
+}
+
+.card:hover {
+box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+padding: 2px 16px;
+}
+</style>
+</head>
+<body>
+<div class="card">
+<div class="container">
+<h1>Greetings</h1>
+<p>This is a test email from :<a href="https://www.google.com">Fahad</a></p> 
+</div>
+</div>
+</body>
+</html>`;
